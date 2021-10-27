678f012802e6c81bf1ec73aed06d24e914c5ea70
MatheusDario <dario.matheus97@gmail.com>
2021-10-27 13:31:48 -0300

    export/import

---
+++ aula94/src/index.js
@@ -0,0 +1,3 @@
+import {nome, sobrenome, idade, soma} from './modulo1';
+console.log(nome, sobrenome, idade);
+console.log(soma(5,5));
---
+++ aula94/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Matheus';
+const sobrenome = 'Dario';
+const idade = 24;
+
+function soma(x, y) {
+    return x + y;
+}
+
+export {nome, sobrenome, idade, soma};
