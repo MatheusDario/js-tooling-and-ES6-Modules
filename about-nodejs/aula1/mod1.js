c525e4959cdf979821cfb2ce26183cd24ba119c7
MatheusDario <dario.matheus97@gmail.com>
2021-11-01 17:22:54 -0300

    inicio about node modules

---
+++ about-nodejs/aula1/mod1.js
@@ -0,0 +1,10 @@
+const nome = 'Matheus';
+const sobrenome = 'Dario';
+
+const falaNome = () => nome + ' ' + sobrenome;
+
+exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
+
