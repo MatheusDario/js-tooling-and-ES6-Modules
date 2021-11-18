c07e04758e6fb7b7f0724fa5a49fb40c12eaaa21
MatheusDario <dario.matheus97@gmail.com>
2021-11-18 12:40:12 -0300

    servidor node

---
+++ about-nodejs/aula6/server.js
@@ -0,0 +1,15 @@
+const { request, response } = require('express');
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+    response.send('Hello world!');
+});
+
+app.get('/contato', (request, response) => {
+    response.send('Obrigado por entrar em contato com a gente!')
+});
+
+app.listen(3000, () => {
+    console.log('Servidor Executando na porta 3000');
+});
+
