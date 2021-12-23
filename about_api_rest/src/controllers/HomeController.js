53c39c6ed5209b4316e45bcbdd0a3ade48acb565
MatheusDario <dario.matheus97@gmail.com>
2021-12-23 13:53:55 -0300

    sequelize configurado

--- about_api_rest/src/controllers/HomeController.js
+++ about_api_rest/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Matheus',
+      sobrenome: 'Dario',
+      email: 'dario.matheus97@gmail.com',
+      idade: 24,
+      peso: 85.00,
+      altura: 1.80,
     });
+    res.json(novoAluno);
   }
 }
 
