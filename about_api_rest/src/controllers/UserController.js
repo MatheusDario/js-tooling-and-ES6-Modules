6e460871c7fd1fb6be86a36e6f7c3f28f616c616
MatheusDario <dario.matheus97@gmail.com>
2021-12-24 14:43:59 -0300

    criando crud dos users

--- about_api_rest/src/controllers/UserController.js
+++ about_api_rest/src/controllers/UserController.js
@@ -1,12 +1,88 @@
 import User from '../models/User';
 
 class UserController {
+  // create
   async create(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Show
+  async show(req, res) {
+    try {
+      const users = await User.findByPk(req.params.id);
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // Update
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID.'],
+        });
+      }
+
+      const users = await User.findByPk(req.params.id);
+
+      if (!users) {
+        return res.status(400).json({
+          errors: ['User dont exist.'],
+        });
+      }
+
+      const novosDados = await users.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['Missing ID.'],
+        });
+      }
+
+      const users = await User.findByPk(req.params.id);
+
+      if (!users) {
+        return res.status(400).json({
+          errors: ['User dont exist.'],
+        });
+      }
+
+      await users.destroy();
+      return res.json(users);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
