1ff9b885c91b3b83e8fa18b6f5eafc066b1ea383
MatheusDario <dario.matheus97@gmail.com>
2021-12-24 13:57:57 -0300

    criando usuario e validamos

---
+++ about_api_rest/src/models/User.js
@@ -0,0 +1,53 @@
+import bcryptjs from 'bcryptjs';
+import Sequelize, { Model } from 'sequelize';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email ja existe',
+        },
+        validate: {
+          isEmail: {
+            msg: 'O E-mail digitado está inválido',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [5, 15],
+            msg: 'Campo Senha deve ter entre 6 e 15 caracteres',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+
+    return this;
+  }
+}
