93f26c0d0988efbd1c16d6ac59c427df62be4780
MatheusDario <dario.matheus97@gmail.com>
2021-12-28 14:03:38 -0300

    criando rotas alunos

--- about_api_rest/src/controllers/HomeController.js
+++ about_api_rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Matheus',
-      sobrenome: 'Dario',
-      email: 'dario.matheus97@gmail.com',
-      idade: 24,
-      peso: 85.00,
-      altura: 1.80,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
